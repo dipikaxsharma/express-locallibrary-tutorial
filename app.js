35a765a169505fb20496add78cf75646065a9942
dipika sharma <sharmadipika55@hotmail.com>
2020-04-10 20:09:17 -0500

    final commit

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://dipikaxsharma:<password>@cluster0-y7bom.mongodb.net/test?retryWrites=true&w=majorityur_database_url_here';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
