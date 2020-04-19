f38a2d958903f574c068dde4a75a077c48c7c8c7
dipika sharma <sharmadipika55@hotmail.com>
2020-04-19 14:37:10 -0500

    final .ev-example submission

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ var app = express();
 dotenv.config({ path: '.env' })
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://dipikaxsharma:<password>@cluster0-y7bom.mongodb.net/test?retryWrites=true&w=majorityur_database_url_here';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+var mongoDB = process.env.ATLAS_URI;
+mongoose.connect(mongoDB, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ });
+ 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
